a9a3941bdbd91a1d732ae4f9e9eb1b19adb7a3fd
Torrey Leonard <torreyleonard@voxela.com>
2019-11-21 01:06:28 -0500

    Updates

---
+++ precache-manifest.a30db2bfd38194fc04c0a84f7737c228.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646771d8206fb9a6a8008344cd8adbd2",
+    "url": "/desinr/index.html"
+  },
+  {
+    "revision": "1a134709036a0886bbe5",
+    "url": "/desinr/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4ec453e86a5ffd8c49fd",
+    "url": "/desinr/static/css/main.ad99bc77.chunk.css"
+  },
+  {
+    "revision": "1a134709036a0886bbe5",
+    "url": "/desinr/static/js/2.41a23b1f.chunk.js"
+  },
+  {
+    "revision": "4ec453e86a5ffd8c49fd",
+    "url": "/desinr/static/js/main.82ec3e13.chunk.js"
+  },
+  {
+    "revision": "8a9d1f9ff8f6d2fdc39e",
+    "url": "/desinr/static/js/runtime-main.063c15ee.js"
+  },
+  {
+    "revision": "37e9331c6d0b33b6eef182b28d5f62cf",
+    "url": "/desinr/static/media/alexa-gif.37e9331c.gif"
+  },
+  {
+    "revision": "4a1db3f53d8c46fefbd2f9093d16244b",
+    "url": "/desinr/static/media/alexa.4a1db3f5.png"
+  },
+  {
+    "revision": "a760194ced72846764c86c778eaf91c1",
+    "url": "/desinr/static/media/amazon-logo.a760194c.png"
+  },
+  {
+    "revision": "6130e19fe40559ecadf702b7c5e46c5d",
+    "url": "/desinr/static/media/amazon-redesign.6130e19f.png"
+  },
+  {
+    "revision": "e11ffe4a661c906082aa192c78e13c13",
+    "url": "/desinr/static/media/black-and-decker.e11ffe4a.jpg"
+  },
+  {
+    "revision": "054895abf3fa90063451fb0d07a8ab9d",
+    "url": "/desinr/static/media/cover.054895ab.jpg"
+  },
+  {
+    "revision": "c0907acb75cba62fda159774f60b364c",
+    "url": "/desinr/static/media/fox-icon.c0907acb.png"
+  },
+  {
+    "revision": "43498feeb613719883b032d6ee89d1fb",
+    "url": "/desinr/static/media/great-pumpkin.43498fee.gif"
+  },
+  {
+    "revision": "69c6f52ae0953eaf6c73186ead1b7791",
+    "url": "/desinr/static/media/lametric-time.69c6f52a.jpg"
+  },
+  {
+    "revision": "07b6637f8ef8580ec419297acb4c0c0e",
+    "url": "/desinr/static/media/lametric.07b6637f.png"
+  },
+  {
+    "revision": "fd30b02ad915e1f5e456eb65bc4e4439",
+    "url": "/desinr/static/media/screenshot-1.fd30b02a.png"
+  },
+  {
+    "revision": "499d5d2560fe95bfdbbedaacf2be7a08",
+    "url": "/desinr/static/media/screenshot-2.499d5d25.png"
+  },
+  {
+    "revision": "3e7620b1c18c027e335031a366e8b414",
+    "url": "/desinr/static/media/screenshot-3.3e7620b1.png"
+  },
+  {
+    "revision": "13eac127845fb32c17a3e24b518139e6",
+    "url": "/desinr/static/media/video-1.13eac127.gif"
+  },
+  {
+    "revision": "6ab5c50991304b484ad97e1257078499",
+    "url": "/desinr/static/media/video-2.6ab5c509.gif"
+  },
+  {
+    "revision": "2f54e16b1f38c34ee111f94e79068a22",
+    "url": "/desinr/static/media/video-mapping.2f54e16b.gif"
+  },
+  {
+    "revision": "f92beacee1ce087e4ae775a2252c603e",
+    "url": "/desinr/static/media/video-statusbar.f92beace.gif"
+  },
+  {
+    "revision": "008ea9f0a7b1a7ccf51400deb9a73031",
+    "url": "/desinr/static/media/where-gif.008ea9f0.gif"
+  }
+]);
